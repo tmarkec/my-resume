56ee846bb7d893b116395933001978f973699dec
tmarkec <tmarkec@gmail.com>
2023-02-14 13:37:06 +0000

    fix bug sending email

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -1,8 +1,8 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
+    emailjs.send("service_fahse4j", "markec", {
+        "project_request": contactForm.projectsummary.value,
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "from_email": contactForm.emailaddress.value
     })
     .then(
         function(response) {
